e913ce354c3bac801539101eb89c7b29b6d49d3c
Pawan Kumar Mishra <63885768+pawankm21@users.noreply.github.com>
2022-07-26 23:42:14 +0530

    add logout

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -2,18 +2,28 @@ import Column from "./column"
 import { ReactComponent as SearchIcon } from "../../assets/search.svg";
 import { ReactComponent as FilterIcon } from "../../assets/filter.svg";
 import Expand from "./expand";
+import { useSelector ,useDispatch} from "react-redux";
 export default function Kanban() {
+    const user = useSelector(state => state.auth.user.name)
+    const dispatch = useDispatch();
+    function handleLogout() {
+        dispatch(logout());
+    }
     return (
         <div className="w-full h-fit bg-[#FEFEFE] px-9 pt-16 overflow-hidden ">
 
             <div className="grid grid-cols-3 mb-20 ">
-                <div>
+                <div className="col-span-2">
                     <div className="flex items-center gap-3.5">
                         <SearchIcon className="w-3.5 h-3.5" />
                         <textarea rows={1} type="text" name="" placeholder="Search" className="resize-none outline-none" />
                     </div>
                 </div>
-                <div></div>
+                <div className="flex gap-4 justify-self-end items-center justify-end w-full" onClick={handleLogout}> 
+                    <h3>Hi {user||"Anonymous"}</h3>
+                    <button><img src="/profile.png" className='w-8 h-8 rounded-full border-2 border-gray-400' /></button>
+                    
+                </div>
             </div>
             <div className="grid grid-cols-2">
                 <h1 className="font-medium text-2xl">Projects</h1>
